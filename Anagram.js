4a6bc717faf46fea7259b07971d4270ccee5d3da
fjp-pjf <feminjustin07@gmail.com>
2025-10-04 23:17:55 +0530

    anagram find

---
+++ Anagram.js
@@ -0,0 +1,25 @@
+class Solution {
+  /**
+   * @param {string} s
+   * @param {string} t
+   * @return {boolean}
+   */
+  isAnagram(s, t) {
+    if (s.length !== t.length) return false;
+
+    const count = new Map();
+    for (let char of s) {
+      count.set(char, (count.get(char) || 0) + 1);
+    }
+
+    for (let char of t) {
+      if (!count.has(char)) return false;
+
+      count.set(char, count.get(char) - 1);
+
+      if (count.get(char) < 0) return false;
+    }
+
+    return true;
+  }
+}

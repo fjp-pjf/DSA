575821a5f69e2ef1840bb3693636877888f18f9e
fjp-pjf <feminjustin07@gmail.com>
2025-10-23 23:06:54 +0530

    isPalindrome logic

---
+++ Palindrome.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isPalindrome(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    while (left < right && !isAlphaNumeric(s[left])) left++;
+    while (left < right && !isAlphaNumeric(s[right])) right--;
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+function isAlphaNumeric(ch) {
+  const code = ch.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) || // 0–9
+    (code >= 65 && code <= 90) || // A–Z
+    (code >= 97 && code <= 122) // a–z
+  );
+}
